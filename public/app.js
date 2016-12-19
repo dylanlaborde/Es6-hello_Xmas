d0d5a840be6124b481a995dae70ea89a1d36d11c
Dylan <dylan4047@gmail.com>
2016-12-19 16:51:24 +0100

    etape 1 fait

--- public/app.js
+++ public/app.js
@@ -1,1 +1,15 @@
 "use strict";
+
+var hello = function hello(person) {
+	if (document.getElementById("nom").value === "") {
+		person = "Père Noël";
+		console.log("empty");
+	} else {
+		person = document.getElementById("nom").value;
+	}
+	document.getElementById("sayIt").innerHTML = 'Bonjour ' + person;
+	console.log('Bonjour ' + person);
+};
+document.getElementById("val").addEventListener("click", function () {
+	hello();
+});
---
+++ src/app.es6.js
@@ -0,0 +1,19 @@
+let hello =(person)=>{
+	if (document.getElementById("nom").value === "") {
+		person = "Père Noël"
+		console.log("empty")
+	}
+	else{
+		person = document.getElementById("nom").value
+		
+	}
+	document.getElementById("sayIt").innerHTML = 'Bonjour ' + person
+	console.log('Bonjour '+person);
+}
+document.getElementById("val").addEventListener("click", ()=>{
+	hello();
+})
+
+
+
+
