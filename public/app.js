7b061cc0e767edce95c82a434ea4d603996a7d01
Dylan <dylan4047@gmail.com>
2016-12-20 12:00:34 +0100

    etape 2 terminer + empty input

--- public/app.js
+++ public/app.js
@@ -1,15 +1,37 @@
 "use strict";
 
-var hello = function hello(person) {
-	if (document.getElementById("nom").value === "") {
-		person = "Père Noël";
-		console.log("empty");
-	} else {
-		person = document.getElementById("nom").value;
-	}
-	document.getElementById("sayIt").innerHTML = 'Bonjour ' + person;
-	console.log('Bonjour ' + person);
-};
 document.getElementById("val").addEventListener("click", function () {
 	hello();
 });
+var person = undefined;
+var mytab = [];
+
+var hello = function hello(person) {
+
+	if (document.getElementById("nom").value === "") {
+		person = "Père Noël";
+	} else {
+		person = document.getElementById("nom").value;
+	}
+
+	var say = "Bonjour " + person;
+	var insertSay = document.getElementById("sayIt").innerHTML = say;
+	console.log(insertSay);
+	console.log(say);
+	mytab.push(say);
+	console.log(mytab);
+	// let test = document.createElement('li').innerHTML = mytab[i]
+	// console.log(document.createElement('li')+ mytab.length)
+	// document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = mytab
+
+	// document.getElementById("historique").innerHTML = mytab.length
+	document.getElementById("nom").value = "";
+	document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = say;
+
+	// for (var i = 0; i < mytab.length; i++) {
+	// 	 console.log(mytab[i])
+	//   document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = mytab[i]
+	//  // document.getElementById("historique")
+	// 	// console.log(mytab.length -1)
+	// }
+};
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,18 +1,48 @@
+document.getElementById("val").addEventListener("click", ()=>{
+	hello();
+})
+let person = undefined
+let mytab = []
+
+
 let hello =(person)=>{
+
 	if (document.getElementById("nom").value === "") {
 		person = "Père Noël"
-		console.log("empty")
 	}
 	else{
 		person = document.getElementById("nom").value
 		
 	}
-	document.getElementById("sayIt").innerHTML = 'Bonjour ' + person
-	console.log('Bonjour '+person);
+	
+	const say = `Bonjour ${person}` 
+	const insertSay = document.getElementById("sayIt").innerHTML = say 
+	 console.log(insertSay)
+	 console.log(say)
+	 mytab.push(say)
+	 console.log(mytab)
+		// let test = document.createElement('li').innerHTML = mytab[i]
+	 // console.log(document.createElement('li')+ mytab.length)
+	 // document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = mytab
+
+	// document.getElementById("historique").innerHTML = mytab.length
+document.getElementById("nom").value = ""
+	  document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = say
+
+	// for (var i = 0; i < mytab.length; i++) {
+	// 	 console.log(mytab[i])
+	//   document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = mytab[i]
+	//  // document.getElementById("historique")
+	// 	// console.log(mytab.length -1)
+	// }
+
 }
-document.getElementById("val").addEventListener("click", ()=>{
-	hello();
-})
+
+
+
+
+
+
 
 
 
