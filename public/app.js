02cd38ed0541aa930639688bae042a7c46f28e9e
Dylan <dylan4047@gmail.com>
2016-12-21 15:04:34 +0100

    filtre terminer

--- public/app.js
+++ public/app.js
@@ -1,14 +1,29 @@
 "use strict";
 
-// for (var i = 0; i < mytab.length; i++) {
-// 	let mytabFor = mytab[i]
-// 	 // console.log(mytab[i].nom)
-// 	 console.log(mytab[i])
-// 	 console.log(i)
-// 	 console.log(mytabFor[0]);
-// 	 // if (true) {}
-// }
+document.getElementById("resetSearch").addEventListener("click", function () {
+	document.getElementById("resultSearch").innerHTML = "";
+	console.log("lalalalal");
+});
 
+document.getElementById("valSearch").addEventListener("click", function () {
+	document.getElementById("resultSearch").innerHTML = "";
+
+	console.log(document.getElementById("search").value);
+	console.log(mytab);
+	var tableau = ["dylan", "rudy", "arsene", "pere Noël", "philipe", "axel"];
+	// if (document.getElementById("search").value === "") {
+	// 	console.log("erreurs")
+	// }
+	var élément = document.getElementById("search").value;
+
+	for (var i = 0; i < mytab.length; i++) {
+		if (mytab[i].indexOf(élément) === 0) {
+			console.log(mytab[i]);
+			document.getElementById("resultSearch").appendChild(document.createElement('li')).innerHTML = mytab[i];
+		}
+		document.getElementById("search").value = "";
+	}
+});
 
 document.getElementById("val").addEventListener("click", function () {
 
@@ -16,38 +31,33 @@ document.getElementById("val").addEventListener("click", function () {
 	console.log(mytab);
 });
 
-document.getElementById("valSearch").addEventListener("click", function () {
-	var valueSearch = document.getElementById("search").value;
-	// console.log(valueSearch)
-	console.log(mytab);
-	for (var i = 0; i < mytab.length; i++) {
-		var _mytabFor = mytab[i];
-		// console.log(mytabFor.length)
-		console.log(mytab[1][i]);
+// --------->>> filtre a la premier letre (1 er methode utiliser)  --------->>>>>>>>>
+// ---------->>>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^----------->>>>>>>>
 
-		// console.log(mytab[i].nom)
-		// console.log(mytab[i][0])
+// document.getElementById("valSearch").addEventListener("click", ()=>{
+
+// 	let valueSearch = document.getElementById("search").value
+
+// 	console.log(mytab)
+// 	for (var i = 0; i < mytab.length; i++) {
+// 		let mytabFor = mytab[i]
+// 		console.log(mytab[1][i])
+// 		if (mytab[i][0] === valueSearch) {
+// 			document.getElementById("resultSearch").appendChild(document.createElement('li')).innerHTML = mytab[i]
 
 
-		// console.log(valueSearch)
-		// console.log()
-		if (mytab[i][0] === valueSearch) {
-			// document.getElementById("resultSearch").innerHTML = mytab
-			// document.getElementById("resultSearch").innerHTML = mytab[i]
-			document.getElementById("resultSearch").appendChild(document.createElement('li')).innerHTML = mytab[i];
+// 			console.log("yep trouver")
+// 			console.log(i)
+// 			console.log(mytab[i])
+// 		}
 
-			// document.getElementById("resultSearch").innerHTML = mytab[i]
-			console.log("yep trouver");
-			console.log(i);
-			console.log(mytab[i]);
-		}
-		// else if (mytab[i][0] !== valueSearch) {
-		// 		console.log("")
-		//  // document.getElementById("resultSearch").innerHTML = "Aucun resultat !"
-		// }
-		document.getElementById("search").value = "";
-	}
-});
+// 		document.getElementById("search").value = ""
+
+// 	}
+// })
+// ---------->>>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^----------->>>>>>>>
+// --------->>> filtre a la premier letre (1 er methode utiliser)  --------->>>>>>>>>
+// ---------->>>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^----------->>>>>>>>
 
 var person = undefined;
 var mytab = ["dylan", "rudy", "arsene", "pere Noël", "philipe", "axel"];
@@ -62,9 +72,7 @@ var hello = function hello(person) {
 	var say = "Bonjour " + person;
 	var nom = "" + person;
 	var insertSay = document.getElementById("sayIt").innerHTML = say;
-	// console.log(insertSay)
-	// console.log(say)
-	// console.log(mytab)
+
 	mytab.push(nom);
 
 	var historique = document.getElementById("historique");
@@ -72,13 +80,5 @@ var hello = function hello(person) {
 
 	historique.appendChild(li).innerHTML = nom;
 
-	for (var i = 0; i < nom.length; i++) {
-
-		// console.log(nom[i])
-	}
-
 	document.getElementById("nom").value = "";
-
-	// document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = nom
-
 };
--- src/app.es6.js
+++ src/app.es6.js
@@ -1,74 +1,82 @@
+document.getElementById("resetSearch").addEventListener("click", ()=>{
+	document.getElementById("resultSearch").innerHTML = ""
+	console.log("lalalalal")
+})
 
-	// for (var i = 0; i < mytab.length; i++) {
-	// 	let mytabFor = mytab[i]
-	// 	 // console.log(mytab[i].nom)
-	// 	 console.log(mytab[i])
-	// 	 console.log(i)
-	// 	 console.log(mytabFor[0]);
-	// 	 // if (true) {}
-	// }
+document.getElementById("valSearch").addEventListener("click", ()=>{
+	document.getElementById("resultSearch").innerHTML = ""
 
-	
-
-	document.getElementById("val").addEventListener("click", ()=>{
-
-		hello();
-		console.log(mytab)
-	})
-
-	document.getElementById("valSearch").addEventListener("click", ()=>{
-		let valueSearch = document.getElementById("search").value
-	// console.log(valueSearch)
+	console.log(document.getElementById("search").value)
 	console.log(mytab)
-	for (var i = 0; i < mytab.length; i++) {
-		let mytabFor = mytab[i]
-		// console.log(mytabFor.length)
-		console.log(mytab[1][i])
+	var tableau = ["dylan", "rudy","arsene", "pere Noël" , "philipe" , "axel"]
+		// if (document.getElementById("search").value === "") {
+		// 	console.log("erreurs")
+		// }
+		var élément = document.getElementById("search").value
+		
 
-		 // console.log(mytab[i].nom)
-		 // console.log(mytab[i][0])
-
-
-
-		 // console.log(valueSearch)
-		 // console.log()
-		 if (mytab[i][0] === valueSearch) {
-		 	// document.getElementById("resultSearch").innerHTML = mytab
-		 	// document.getElementById("resultSearch").innerHTML = mytab[i]
-		 	document.getElementById("resultSearch").appendChild(document.createElement('li')).innerHTML = mytab[i]
-
-		 	// document.getElementById("resultSearch").innerHTML = mytab[i]
-		 	console.log("yep trouver")
-		 	console.log(i)
-		 	console.log(mytab[i])
-		 }
-		  // else if (mytab[i][0] !== valueSearch) {
-		  // 		console.log("")
-		 	//  // document.getElementById("resultSearch").innerHTML = "Aucun resultat !"
-		  // }
-		  document.getElementById("search").value = ""
-
+		for (var i = 0; i < mytab.length; i++) {
+		if (mytab[i].indexOf(élément) === 0) {
+			console.log(mytab[i]);
+			document.getElementById("resultSearch").appendChild(document.createElement('li')).innerHTML = mytab[i];
 		}
+ 		document.getElementById("search").value = ""
+	}
+
 	})
 
-	let person = undefined
-	let mytab = ["dylan", "rudy","arsene", "pere Noël" , "philipe" , "axel"]
-	let mytabFor = undefined
-	let hello =(person)=>{
-		if (document.getElementById("nom").value === "") {
-			person = "Père Noël"
-		}
-		else{
-			person = document.getElementById("nom").value
+document.getElementById("val").addEventListener("click", ()=>{
 
-		}
+	hello();
+	console.log(mytab)
+})
 
-		const say = `Bonjour ${person}` 
-		const nom = `${person}` 
-		const insertSay = document.getElementById("sayIt").innerHTML = say 
-	// console.log(insertSay)
-	// console.log(say)
-	// console.log(mytab)
+
+
+// --------->>> filtre a la premier letre (1 er methode utiliser)  --------->>>>>>>>>
+// ---------->>>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^----------->>>>>>>>
+
+// document.getElementById("valSearch").addEventListener("click", ()=>{
+
+// 	let valueSearch = document.getElementById("search").value
+	
+// 	console.log(mytab)
+// 	for (var i = 0; i < mytab.length; i++) {
+// 		let mytabFor = mytab[i]
+// 		console.log(mytab[1][i])
+// 		if (mytab[i][0] === valueSearch) {
+// 			document.getElementById("resultSearch").appendChild(document.createElement('li')).innerHTML = mytab[i]
+
+
+// 			console.log("yep trouver")
+// 			console.log(i)
+// 			console.log(mytab[i])
+// 		}
+
+// 		document.getElementById("search").value = ""
+
+// 	}
+// })
+// ---------->>>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^----------->>>>>>>>
+// --------->>> filtre a la premier letre (1 er methode utiliser)  --------->>>>>>>>>
+// ---------->>>^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^----------->>>>>>>>
+
+let person = undefined
+let mytab = ["dylan", "rudy","arsene", "pere Noël" , "philipe" , "axel"]
+let mytabFor = undefined
+let hello =(person)=>{
+	if (document.getElementById("nom").value === "") {
+		person = "Père Noël"
+	}
+	else{
+		person = document.getElementById("nom").value
+
+	}
+
+	const say = `Bonjour ${person}` 
+	const nom = `${person}` 
+	const insertSay = document.getElementById("sayIt").innerHTML = say 
+	
 	mytab.push(nom)
 
 	let historique = document.getElementById("historique")
@@ -76,21 +84,9 @@
 
 	historique.appendChild(li).innerHTML = nom
 
-	for (var i = 0; i < nom.length; i++) {
-
-		// console.log(nom[i])
-	}
 	
-
-
-
 	document.getElementById("nom").value = ""
 	
-	
-
-	// document.getElementById("historique").appendChild(document.createElement('li')).innerHTML = nom
-
-
 
 }
 
